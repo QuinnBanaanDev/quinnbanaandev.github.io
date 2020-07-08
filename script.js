aaff78424e290cfea26a43588962d7e335726818
QuinnBanaan <56636906+QuinnBanaanDev@users.noreply.github.com>
2020-07-08 14:44:13 +0200

    i added everything
    lala

---
+++ script.js
@@ -0,0 +1,7 @@
+var koekie;
+function check_login(form){
+    if (form.userid.value === "QuinnBanaan" && form.passwrd.value === "Quinnie"){
+        window.open("MakeYourOwn.html");
+    } else alert("Username and Password don't match"); koekie.innerHTML = "password incorrect";
+}
+
